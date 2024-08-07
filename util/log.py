8453063451620d0686514a8cce4a16b744fe3adf
HBcao233 <hbcaoqaq@gmail.com>
2024-08-08 03:27:17 +0800

    feat: set level of logger "telethon.extensions.messagepacker" to ERROR

--- util/log.py
+++ util/log.py
@@ -5,6 +5,7 @@ import config
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("telethon.network.mtprotosender").setLevel(logging.ERROR)
+logging.getLogger("telethon.extensions.messagepacker").setLevel(logging.ERROR)
 
 
 logging.basicConfig(
