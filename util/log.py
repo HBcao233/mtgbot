c66af429d64809f4ce85bde453ae6aaab6c9ad90
HBcao233 <hbcaoqaq@gmail.com>
2024-08-28 08:21:53 +0800

    feat: 修改日志文件名格式

--- util/log.py
+++ util/log.py
@@ -1,14 +1,37 @@
 import logging
-import sys, os, re
+import sys, os, re, time
 import config
 from logging.handlers import TimedRotatingFileHandler
 
 __all__ = ["logger"]
 
-
 logs_dir = os.path.join(config.botRoot, 'logs/')
 if not os.path.isdir(logs_dir):
   os.mkdir(logs_dir)
+
+
+class TimedHandler(TimedRotatingFileHandler):
+  def __init__(self, 
+    backupCount=30,
+    encoding=None, delay=False,
+    errors=None
+  ):
+    suffix = "%Y-%m-%d.log"
+    t = int(time.time())
+    timeTuple = time.localtime(t)
+    filename = time.strftime(suffix, timeTuple)
+    filename = os.path.join(logs_dir, filename)
+    super().__init__(filename, 'MIDNIGHT', 1, backupCount, encoding=encoding, delay=delay, utc=False, atTime=None, errors=errors)
+    self.suffix = suffix
+    self.extMatch = re.compile(r"(?<=\.)\d{4}-\d{2}-\d{2}\.log$", re.ASCII)
+  
+  def namer(self, name):
+    _path, _name = os.path.split(name)
+    if name.count('.') <= 1:
+      return name
+    _name = _name[_name.index('.log.')+5:]
+    return os.path.join(_path, _name)
+
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("telethon.client.updates").setLevel(logging.ERROR)
@@ -27,9 +50,7 @@ default_handler = logging.StreamHandler(sys.stdout)
 default_handler.setFormatter(
   logging.Formatter("[%(asctime)s %(name)s] %(levelname)s: %(message)s")
 )
-file_handler = TimedRotatingFileHandler(filename=os.path.join(logs_dir, 'mtgbot.log'), when="MIDNIGHT", interval=1, backupCount=30)
-file_handler.suffix = "%Y-%m-%d.log"
-file_handler.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}.log$")
+file_handler = TimedHandler()
 file_handler.setFormatter(
   logging.Formatter("[%(asctime)s %(name)s] %(levelname)s: %(message)s")
 )
